489496b9734e074260757a50d0c1220fb8b440a5
Mossaab Bagdouri <mossaab@hotmail.com>
2013-07-12 18:04:05 -0400

    .

--- src/java/main/ivory/app/PreprocessTrecForeign.java
+++ src/java/main/ivory/app/PreprocessTrecForeign.java
@@ -70,10 +70,10 @@ public class PreprocessTrecForeign extends Configured implements Tool {
     conf.setInt(Constants.MaxDf, Integer.MAX_VALUE);
     conf.setInt(Constants.TermIndexWindow, 8);
     conf.set(Constants.InputFormat, TrecDocumentInputFormat.class.getCanonicalName());
-    conf.set("mapreduce.map.memory.mb", "4096");
-    conf.set("mapreduce.map.java.opts", "-Xmx4096m");
-    conf.set("mapreduce.reduce.memory.mb", "4096");
-    conf.set("mapreduce.reduce.java.opts", "-Xmx4096m");
+    conf.set("mapreduce.map.memory.mb", "1024");
+    conf.set("mapreduce.map.java.opts", "-Xmx1024m");
+    conf.set("mapreduce.reduce.memory.mb", "1024");
+    conf.set("mapreduce.reduce.java.opts", "-Xmx1024m");
 
     new BuildTermDocVectors(conf).run();
     // new ComputeGlobalTermStatistics(conf).run();
