e4acbca776b0244ad3fe29a612bb8862d16abf99
Mossaab Bagdouri <mossaab@hotmail.com>
2013-07-14 17:28:46 -0400

    .

--- src/java/main/ivory/core/index/BuildIPInvertedIndexDocSorted.java
+++ src/java/main/ivory/core/index/BuildIPInvertedIndexDocSorted.java
@@ -279,10 +279,10 @@ public class BuildIPInvertedIndexDocSorted extends PowerTool {
 
     conf.setInt("mapred.min.split.size", minSplitSize);
     //conf.set("mapred.child.java.opts", "-Xmx" + maxHeap + "m");
-    conf.set("mapreduce.map.memory.mb", "4096");
-    conf.set("mapreduce.map.java.opts", "-Xmx4096m");
-    conf.set("mapreduce.reduce.memory.mb", "4096");
-    conf.set("mapreduce.reduce.java.opts", "-Xmx4096m");
+    conf.set("mapreduce.map.memory.mb", "6144");
+    conf.set("mapreduce.map.java.opts", "-Xmx6144m");
+    conf.set("mapreduce.reduce.memory.mb", "6144");
+    conf.set("mapreduce.reduce.java.opts", "-Xmx6144m");
 
     Job job = Job.getInstance(conf,
         BuildIPInvertedIndexDocSorted.class.getSimpleName() + ":" + collectionName);
