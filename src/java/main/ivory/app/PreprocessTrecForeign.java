5fc042cfbf2809f2f871ee59c9afaaa92c3dd029
Mossaab Bagdouri <mossaab@hotmail.com>
2013-08-07 12:09:19 -0400

    .

--- src/java/main/ivory/app/PreprocessTrecForeign.java
+++ src/java/main/ivory/app/PreprocessTrecForeign.java
@@ -61,7 +61,7 @@ public class PreprocessTrecForeign extends Configured implements Tool {
 
     RetrievalEnvironment env = new RetrievalEnvironment(indexRootPath, fs);
     Path mappingFile = env.getDocnoMappingData();
-    new TrecDocnoMappingBuilder().build(new Path(collection), mappingFile, conf);
+    //new TrecDocnoMappingBuilder().build(new Path(collection), mappingFile, conf);
 
     conf.set(Constants.DocnoMappingClass, TrecDocnoMapping.class.getCanonicalName());
     conf.set(Constants.DocnoMappingFile, env.getDocnoMappingData().toString());
@@ -71,11 +71,11 @@ public class PreprocessTrecForeign extends Configured implements Tool {
     conf.setInt(Constants.TermIndexWindow, 8);
     conf.set(Constants.InputFormat, TrecDocumentInputFormat.class.getCanonicalName());
 
-    new BuildTermDocVectors(conf).run();
-    new ComputeGlobalTermStatistics(conf).run();
-    new BuildDictionary(conf).run();
-    new BuildIntDocVectors(conf).run();
-    //new BuildIntDocVectorsForwardIndex(conf).run();
+    //new BuildTermDocVectors(conf).run();
+    //new ComputeGlobalTermStatistics(conf).run();
+    //new BuildDictionary(conf).run();
+    //new BuildIntDocVectors(conf).run();
+    new BuildIntDocVectorsForwardIndex(conf).run();
     
     //new BuildTermDocVectorsForwardIndex(conf).run();
 
