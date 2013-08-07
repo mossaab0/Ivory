ca652a6b4100ca32d6ca2d63be8e7108d24197d6
Mossaab Bagdouri <mossaab@hotmail.com>
2013-08-07 11:18:42 -0400

    .

--- src/java/main/ivory/app/PreprocessTrecForeign.java
+++ src/java/main/ivory/app/PreprocessTrecForeign.java
@@ -74,9 +74,9 @@ public class PreprocessTrecForeign extends Configured implements Tool {
     //new BuildTermDocVectors(conf).run();
     //new ComputeGlobalTermStatistics(conf).run();
     //new BuildDictionary(conf).run();
-    new BuildIntDocVectors(conf).run();
-    /*new BuildIntDocVectorsForwardIndex(conf).run();
-    new BuildTermDocVectorsForwardIndex(conf).run();*/
+    //new BuildIntDocVectors(conf).run();
+    new BuildIntDocVectorsForwardIndex(conf).run();
+    //new BuildTermDocVectorsForwardIndex(conf).run();
 
     return 0;
   }
