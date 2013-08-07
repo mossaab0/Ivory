e4d6b2862ec034c39c88440eb4aedc9fc927d51d
Mossaab Bagdouri <mossaab@hotmail.com>
2013-08-07 17:25:24 -0400

    .

--- src/java/main/ivory/app/PreprocessTrecForeign.java
+++ src/java/main/ivory/app/PreprocessTrecForeign.java
@@ -61,7 +61,7 @@ public class PreprocessTrecForeign extends Configured implements Tool {
 
     RetrievalEnvironment env = new RetrievalEnvironment(indexRootPath, fs);
     Path mappingFile = env.getDocnoMappingData();
-    //new TrecDocnoMappingBuilder().build(new Path(collection), mappingFile, conf);
+    new TrecDocnoMappingBuilder().build(new Path(collection), mappingFile, conf);
 
     conf.set(Constants.DocnoMappingClass, TrecDocnoMapping.class.getCanonicalName());
     conf.set(Constants.DocnoMappingFile, env.getDocnoMappingData().toString());
@@ -75,7 +75,7 @@ public class PreprocessTrecForeign extends Configured implements Tool {
     //new ComputeGlobalTermStatistics(conf).run();
     //new BuildDictionary(conf).run();
     //new BuildIntDocVectors(conf).run();
-    new BuildIntDocVectorsForwardIndex(conf).run();
+    //new BuildIntDocVectorsForwardIndex(conf).run();
     
     //new BuildTermDocVectorsForwardIndex(conf).run();
 
