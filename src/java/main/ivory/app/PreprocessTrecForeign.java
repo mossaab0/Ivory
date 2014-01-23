ccb04feb0373003622676c185add05b323c25145
Mossaab Bagdouri <mossaab@hotmail.com>
2014-01-23 09:17:09 -0500

    .

--- src/java/main/ivory/app/PreprocessTrecForeign.java
+++ src/java/main/ivory/app/PreprocessTrecForeign.java
@@ -72,8 +72,8 @@ public class PreprocessTrecForeign extends Configured implements Tool {
     conf.set(Constants.InputFormat, TrecDocumentInputFormat.class.getCanonicalName());
 
     //new BuildTermDocVectors(conf).run();
-    new ComputeGlobalTermStatistics(conf).run();
-    //new BuildDictionary(conf).run();
+    //new ComputeGlobalTermStatistics(conf).run();
+    new BuildDictionary(conf).run();
     //new BuildIntDocVectors(conf).run();
     //new BuildIntDocVectorsForwardIndex(conf).run();
     
